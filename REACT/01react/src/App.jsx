75bccf743d369005ca7f1ebe0f7834403c1d1e6c
code <kabir.naved.khan@gmail.com>
2024-10-09 16:08:17 +0530

    react 2 commit

--- REACT/01react/src/App.jsx
+++ REACT/01react/src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './component/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
+   let changeName = () => {
+    setCount(count + 10)
+   }
+   let changeNames = () => {
+    // if(count != 0) {
+    // setCount(count - 10)
+    // }
+
+    (count > 0) ? setCount(count - 10) : count
+
+   }
+
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-          Hello world!
-       </h1>
+       <div className='flex justify-center items-center gap-3 pt-5'>
+        <h1 className='text-center bg-red-200 p-2'>{count}</h1>
+        <button onClick={changeName} className='bg-blue-400 rounded-md p-2'>Change</button>
+        <button onClick={changeNames} className='bg-blue-400 rounded-md p-2'>Change</button>
+       </div>
+
+
+    <Home/>
+
     </>
   )
 }
---
+++ REACT/02react/src/nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class hai extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hi, I am Naved Kabir
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default hai
